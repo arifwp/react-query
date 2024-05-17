2ee3d30e781b3ae1737bbabe0d84c595cb9e04ee
arifwp <arifwahyu696@gmail.com>
2024-05-17 11:19:21 +0700

    optimistic updating

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,10 +1,105 @@
-import { Link, Outlet } from 'react-router-dom';
+
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
+import { useQuery, useMutation } from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
+import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import { useState } from 'react';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const params = useParams();
+  const navigate = useNavigate();
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events', params.id],
+    queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
+  })
+
+  const { mutate, isPending: isPendingDelete, isError: isErrorDelete, error: errorDelete } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none'
+      });
+      navigate('/events');
+    }
+  });
+
+  function handleStartDelete() {
+    setIsDeleting(true);
+  }
+
+  function handleStopDelete() {
+    setIsDeleting(false);
+  }
+
+  function handleDelete() {
+    mutate({ id: params.id });
+  }
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = <ErrorBlock title="Failed to load data" message={error.info?.message || 'Failed to fetch event detail'} />;
+  }
+
+  if (data) {
+    const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+
+    content = (
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleStartDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate} @ {data.time}</time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
+      {isDeleting && (
+        <Modal onClose={handleStopDelete}>
+          <h2>Are you sure?</h2>
+          <p>Are you sure to delete event? this action can not be undone</p>
+          <div className='form-actions'>
+            {isPendingDelete && <LoadingIndicator />}
+            {!isPendingDelete && (
+              <>
+                <button onClick={handleStopDelete} className='button-text'>Cancel</button>
+                <button onClick={handleDelete} className='button'>Delete</button>
+              </>
+            )}
+          </div>
+          {isErrorDelete && <ErrorBlock title="Failed to delete" message={errorDelete.info?.message || 'Failed to deleting some event'} />}
+        </Modal>
+      )}
       <Outlet />
       <Header>
         <Link to="/events" className="nav-item">
@@ -12,23 +107,7 @@ export default function EventDetails() {
         </Link>
       </Header>
       <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
+        {content}
       </article>
     </>
   );
